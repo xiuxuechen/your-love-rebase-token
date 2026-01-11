7f2a890a2d27daa933991bad1caef346e29776c6
xxc <xiuxuechen0714@gmail.com>
2026-01-11 10:22:27 +0800

    跨链代币测试结束

--- test/unit/BankTellerTest.t.sol
+++ test/unit/BankTellerTest.t.sol
@@ -61,5 +61,60 @@ contract BankTellerTest is Test {
         vm.stopPrank();
     }
 
-    function testDepositLiner() public {}
+    function testDepositLiner() public {
+        vm.deal(XSX_USER, 3 ether);
+
+        vm.startPrank(XSX_USER);
+        // 第一次存款
+        bankTeller.deposit{value: 1 ether}();
+
+        uint256 startTime = token.getUserDepositTime(XSX_USER);
+        uint256 startBalance = token.balanceOf(XSX_USER);
+        assertEq(startBalance, 1 ether);
+
+        // 第二次存款
+        vm.warp(1 days);
+        bankTeller.deposit{value: 1 ether}();
+        uint256 depositedTime = token.getUserDepositTime(XSX_USER) - startTime;
+        uint256 interest = (1 ether *
+            token.getUserInterestRate(XSX_USER) *
+            depositedTime) / (365 days * 1e18);
+
+        uint256 middleBalance = token.balanceOf(XSX_USER);
+        assertGt(middleBalance, startBalance);
+
+        // 第三次存款
+        vm.warp(1 days);
+        bankTeller.deposit{value: 1 ether}();
+        uint256 endBalance = token.balanceOf(XSX_USER);
+        vm.stopPrank();
+
+        assertApproxEqAbs(
+            endBalance - middleBalance,
+            middleBalance - startBalance,
+            interest
+        );
+    }
+
+    function testRedeemWhenTimePassed() public {
+        // 给银行柜员充点钱，否则没钱付利息
+        vm.deal(address(bankTeller), 10 ether);
+
+        vm.deal(XSX_USER, 1 ether);
+        vm.startPrank(XSX_USER);
+        bankTeller.deposit{value: 1 ether}();
+        uint256 startTime = token.getUserDepositTime(XSX_USER);
+        vm.warp(1 days);
+
+        bankTeller.redeem(type(uint256).max);
+        vm.stopPrank();
+
+        uint256 depositedTime = token.getUserDepositTime(XSX_USER) - startTime;
+        uint256 interest = (1 ether *
+            token.getUserInterestRate(XSX_USER) *
+            depositedTime) / (365 days * 1e18);
+
+        assertEq(XSX_USER.balance, 1 ether + interest);
+        assertEq(address(bankTeller).balance, 10 ether - interest);
+    }
 }
